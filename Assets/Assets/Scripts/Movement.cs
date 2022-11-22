7375e005539b15cf688a50f74050d5b41b91da87
Sanki <michal.sanki@gmail.com>
2022-11-22 02:23:33 +0100

    Character Movement v2

--- Assets/Assets/Scripts/Movement.cs
+++ Assets/Assets/Scripts/Movement.cs
@@ -1,26 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Movement : MonoBehaviour
 {
-    public float speed = 10f;
+    public Camera camera;
+    private RaycastHit hit;
+    private NavMeshAgent agent;
+    private string groundTag = "Ground";
     
     // Start is called before the first frame update
     void Start()
     {
-        
+        agent = GetComponent<NavMeshAgent>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        float horizontalInput = Input.GetAxis("Horizontal");
-        float verticalInput = Input.GetAxis("Vertical");
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = camera.ScreenPointToRay(Input.mousePosition);
 
-        Vector3 directionInput = new Vector3(horizontalInput, verticalInput, 0);
-
-        transform.Translate(directionInput * speed * Time.deltaTime);
-
+            if (Physics.Raycast(ray, out hit, Mathf.Infinity))
+            {
+                if (hit.collider.CompareTag(groundTag))
+                {
+                    agent.SetDestination(hit.point);   
+                }
+            }
+        }
     }
 }
